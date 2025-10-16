130e2e7dab17115ff6d576754af613104819c32c
Jordy Wladimir Ortiz Aquino <jordanopro42@gmail.com>
2025-10-15 20:32:50 -0500

    Refactor choices functions for improved input validation and user feedback

--- Personal/Calculadora Sistemas de Ecuaciones/Calculadora_Sistemas_de_Ecuaciones_3x3.py
+++ Personal/Calculadora Sistemas de Ecuaciones/Calculadora_Sistemas_de_Ecuaciones_3x3.py
@@ -4,31 +4,45 @@ import numpy as np
 choice = None #Declaro Variables
 
 print('Bienvenido a la Calculadora de Sistemas de Ecuaciones 3x3')
+#########################################################################################################################################################################################################################################################################################################################################################################################
+print(f'Salir | 1\n2 Variables | 2\n3 Variables | 3')
+
 def choices1(): #Realiza un bucle donde permite escoger entre 3 valores, para evitar una irrupcion en el codigo se aplica un try-except dentro del bucle donde evalua la excepcion "ValueError" haciendo que sea imposible la continuacion del codigo ingresando un valor incorrecto
     while True:
         try:
-            print(f'Salir | 1\n2 Variables | 2\n3 Variables | 3')
             choice = int(input('Eliga la cantidad de variables: '))
             break
         except ValueError:
-            print('Por favor, escoga un valor en la lista de variables')
-
+            print('Por favor, escoja un valor en la lista de variables: ')
+    return choice
+#########################################################################################################################################################################################################################################################################################################################################################################################
 
 def choices2(ch2): 
+    while True:
+        if ch2 == 1:
+            print('Salio Exitosamente del programa, Su eleccion fue 1') 
+            break
 
-    if ch2 == 1:
-        print('Salio Exitosamente del programa, Su eleccion fue 1')
+        elif ch2 == 2:
+            print('Salio Exitosamente del programa, Su eleccion fue 2')
+            break
 
-    elif ch2 == 2:
-        print('Salio Exitosamente del programa, Su eleccion fue 2')
+        elif ch2 == 3:
+            print('Salio Exitosamente del programa, Su eleccion fue 3')
+            break
 
-    elif ch2 == 3:
-        print('Salio Exitosamente del programa, Su eleccion fue 3')
+        else:
+            print('Por favor, escoja un valor en la lista de variables: ')
+            choices1()
+    return ch2
+#########################################################################################################################################################################################################################################################################################################################################################################################
 
-    else:
-        print('Salio Exitosamente del programa, Su eleccion fue invalida')
 
-choices1()
+
+
+
+
+choice = choices1()
 choices2(choice)
         
 
