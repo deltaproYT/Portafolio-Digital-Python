2a222faae3c203a9e79268985bd9be07bd12f62e
Jordy Wladimir Ortiz Aquino <jordanopro42@gmail.com>
2025-10-15 19:18:40 -0500

    Agregar la calculadora de sistemas de ecuaciones 3x3

---
+++ Personal/Calculadora Sistemas de Ecuaciones/Calculadora_Sistemas_de_Ecuaciones_3x3.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+            
+
+
+print('Bienvenido a la Calculadora de Sistemas de Ecuaciones 3x3')
+choice = int(input('Eliga la cantidad de variables: '))
+
+def choices(choice):
+    while choices() != (1 or 2 or 3):
+        if choices() == 1:
+
+
+#print(type(choice))
