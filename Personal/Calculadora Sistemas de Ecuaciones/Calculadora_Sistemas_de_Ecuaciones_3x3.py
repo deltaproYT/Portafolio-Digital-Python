d29118dd3171433dcb78a96b8d7e67f0b3b01d27
Jordy Wladimir Ortiz Aquino <jordanopro42@gmail.com>
2025-10-15 20:07:12 -0500

    Refactor input handling and choice validation in 3x3 equations calculator

--- Personal/Calculadora Sistemas de Ecuaciones/Calculadora_Sistemas_de_Ecuaciones_3x3.py
+++ Personal/Calculadora Sistemas de Ecuaciones/Calculadora_Sistemas_de_Ecuaciones_3x3.py
@@ -1,14 +1,35 @@
 import numpy as np
 
             
-
+choice = None #Declaro Variables
 
 print('Bienvenido a la Calculadora de Sistemas de Ecuaciones 3x3')
-choice = int(input('Eliga la cantidad de variables: '))
+def choices1():
+    while True:
+        try:
+            print(f'Salir | 1\n2 Variables | 2\n3 Variables | 3')
+            choice = int(input('Eliga la cantidad de variables: '))
+            break
+        except ValueError:
+            print('Por favor, escoga un valor en la lista de variables')
 
-def choices(choice):
-    while choices() != (1 or 2 or 3):
-        if choices() == 1:
 
+def choices2(ch2):
+
+    if ch2 == 1:
+        print('Salio Exitosamente del programa, Su eleccion fue 1')
+
+    elif ch2 == 2:
+        print('Salio Exitosamente del programa, Su eleccion fue 2')
+
+    elif ch2 == 3:
+        print('Salio Exitosamente del programa, Su eleccion fue 3')
+
+    else:
+        print('Salio Exitosamente del programa, Su eleccion fue invalida')
+
+choices1()
+choices2(choice)
+        
 
 #print(type(choice))
