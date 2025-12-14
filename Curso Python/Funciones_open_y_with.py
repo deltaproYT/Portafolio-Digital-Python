02812874f3cae638432cb79e74fffb8ebced8243
Jordy Wladimir Ortiz Aquino <jordanopro42@gmail.com>
2025-12-14 01:40:45 -0500

    Replace user input with predefined text in Funciones_open_y_with.py for consistent file writing.

--- Curso Python/Funciones_open_y_with.py
+++ Curso Python/Funciones_open_y_with.py
@@ -1,12 +1,17 @@
-file_name = 'Texto1.txt'
+from pathlib import Path
 
-with open(file_name, 'w') as text:
-    for _ in range(10):
-        text.write(input('Escriba algo:') + '\n') 
+# Use forward slashes or pathlib to avoid backslash-unicode escape issues on Windows
+file_name = Path("C:/Users/Jordy/Visual Studio Code Principal/Repos/Python-Projects/Curso Python/texto1.txt")
+
+with file_name.open('w', encoding='utf-8') as text:
+    text.write('Hola Mundo\n'
+               'Mi programador es Jordy Wladimir Ortiz Aquino\n'
+               'Soy la primera prueba de una impresion fuera de la consola\n'
+               'y creacion de archivo .txt\n'
+               'Luego probaremos otros metodos e ideas nuevas')
 
 
-
-with open(file_name, 'r') as text:
+with file_name.open('r', encoding='utf-8') as text:
     data = text.read()
     for i in data.split('\n'):
         print(i)
