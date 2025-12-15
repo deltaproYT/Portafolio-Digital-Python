f44ff87ac42894ce6a80f0bd38b0f428f2fa63c5
Jordy Wladimir Ortiz Aquino <jordanopro42@gmail.com>
2025-12-15 09:44:38 -0500

    Refactor file paths and consolidate program files; move program1.py and texto1.txt to 'Funciones open y with' directory, and update content accordingly.

--- Curso Python/Funciones_open_y_with.py
+++ Curso Python/Funciones_open_y_with.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 
-file_name = Path("C:/Users/Jordy/Visual Studio Code Principal/Repos/Python-Projects/Curso Python/programa1.py")
+file_name = Path("C:/Users/Jordy/Visual Studio Code Principal/Repos/Python-Projects/Curso Python/Funciones open y with/programa1.py")
 
 with file_name.open('w', encoding='utf-8') as text:
     text.write('print("Hello World")\nlista = []\nfor i in range(10):\n  lista.append(i)\nprint(lista)')
