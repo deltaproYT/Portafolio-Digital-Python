0f8f287b9de1b911a6a537a9a51d8004bd574db5
Jordy Wladimir Ortiz Aquino <jordanopro42@gmail.com>
2025-12-16 11:47:44 -0500

    Add Employee class with salary and lab_info method to clases_y_objetos.py

--- Curso Python/clases_y_objetos.py
+++ Curso Python/clases_y_objetos.py
@@ -6,7 +6,21 @@ class Person:
 
     def full_info(self):
         return f"Hola {self.first_name} {self.last_name} tu cedula es: {self.CI}"
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
     
+class Employee(Person):
+    def __init__(self, first_name, last_name, CI, salary):
+        super().__init__(first_name, last_name, CI)
+        self.salary = salary
+    
+    def lab_info(self):
+        return f"Hola trabajador {super().full_name()} tu salario actual es: {self.salary}$"
+
 
 person_jordy = Person('Jordy','Ortiz','2450938722')
+employee_jordy = Employee('Jordy','Ortiz','2450938722', '750')
+
 print(person_jordy.full_info())
+print(employee_jordy.full_info())
+print(employee_jordy.lab_info())
